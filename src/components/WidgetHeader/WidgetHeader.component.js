177861029ac17f028d88d9498263c4dfb61c81eb
Kamil Kollman <k.m.kollman@gmail.com>
2019-07-31 23:08:03 +0200

    chore: refactor header component

--- src/components/WidgetHeader/WidgetHeader.component.js
+++ src/components/WidgetHeader/WidgetHeader.component.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import cls from 'classnames';
 import PropTypes from 'prop-types';
 
 import styles from './WidgetHeader.module.scss';
 
 const WidgetHeader = ({ title }) => {
-  return <div className={cls(styles.header)}>{title}</div>;
+  return <div className={styles.header}>{title}</div>;
 };
 
 WidgetHeader.propTypes = {
